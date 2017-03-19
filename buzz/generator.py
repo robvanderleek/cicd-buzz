e2f40976811d71b260fdc4eca424fbc28de88535
Rob van der Leek <r.vanderleek@sig.eu>
2017-03-19 15:35:42 +0100

    Step 1

---
+++ buzz/generator.py
@@ -0,0 +1,23 @@
+import random
+
+buzz = ('continuous testing', 'continuous intergration', 
+    'continuous deployment', 'continuous improvement', 'devops')
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 
+    'seriously')
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+
+def sample(l, n = 1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), 
+        sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
+if __name__ == "__main__":
+    print generate_buzz()
