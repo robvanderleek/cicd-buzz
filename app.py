a72e0141b276689f95b32b78c638e5ab3376a78d
Rob van der Leek <r.vanderleek@sig.eu>
2017-03-19 21:05:06 +0100

    Step 5

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import signal
+from flask import Flask
+from buzz import generator
+
+app = Flask(__name__)
+
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
+@app.route("/")
+def generate_buzz():
+    page = '<html><body><h1>'
+    page += generator.generate_buzz() 
+    page += '</h1></body></html>'
+    return page
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
