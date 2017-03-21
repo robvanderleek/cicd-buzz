adeb87840042436c836e320f08c8113c9972f705
Rob van der Leek <r.vanderleek@sig.eu>
2017-03-21 18:39:13 +0100

    Issue 1

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY requirements.txt /src/requirements.txt
 RUN pip install -r /src/requirements.txt
 COPY app.py /src
 COPY buzz /src/buzz
+COPY static /src/static
+COPY templates /src/templates
 CMD python /src/app.py
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -9,10 +9,8 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz() 
-    page += '</h1></body></html>'
-    return page
+    buzz = generator.generate_buzz()
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
