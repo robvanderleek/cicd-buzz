be997bdac32667338d74c07239f7663d645dfb7f
Rob van der Leek <r.vanderleek@sig.eu>
2017-10-05 16:27:11 +0200

    Improved landing page

--- app.py
+++ app.py
@@ -9,10 +9,42 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz() 
-    page += '</h1></body></html>'
-    return page
+    return """
+    <html>
+      <head>
+        <title>CI/CD buzz generator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Lobster" 
+          rel="stylesheet">
+        <style>
+          body {
+            padding: 0; 
+            background: url('/static/background.jpg') no-repeat;
+            background-size: cover; 
+            background-color: #58576a; 
+            background-blend-mode: multiply; 
+          }
+          .buzz { 
+            color: #fff; 
+            font-family: 'Lobster', cursive; 
+            font-size: 48px; 
+          }
+          div.buzz { 
+            max-width: 50%; 
+            margin: 0 auto; 
+            text-align: center; 
+            position: relative; 
+            top: 40%;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="buzz">
+          <p class="buzz">""" + generator.generate_buzz() + """</p>
+        </div>
+      </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
