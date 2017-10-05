011d6b97fc409ccf73f3a6041c2a5de42816af76
Rob van der Leek <r.vanderleek@sig.eu>
2017-10-05 21:46:43 +0200

    Refactor landing page

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ RUN pip install -r /src/requirements.txt
 COPY app.py /src
 COPY buzz /src/buzz
 COPY static /src/static
+COPY templates /src/templates
 CMD python /src/app.py
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
@@ -9,42 +9,8 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    return """
-    <html>
-      <head>
-        <title>CI/CD buzz generator</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css?family=Lobster" 
-          rel="stylesheet">
-        <style>
-          body {
-            padding: 0; 
-            background: url('/static/background.jpg') no-repeat;
-            background-size: cover; 
-            background-color: #58576a; 
-            background-blend-mode: multiply; 
-          }
-          .buzz { 
-            color: #fff; 
-            font-family: 'Lobster', cursive; 
-            font-size: 48px; 
-          }
-          div.buzz { 
-            max-width: 50%; 
-            margin: 0 auto; 
-            text-align: center; 
-            position: relative; 
-            top: 40%;
-          }
-        </style>
-      </head>
-      <body>
-        <div class="buzz">
-          <p class="buzz">""" + generator.generate_buzz() + """</p>
-        </div>
-      </body>
-    </html>
-    """
+    buzz = generator.generate_buzz() 
+    return render_template('index.html', buzz=buzz)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
