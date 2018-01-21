bd906a396ce0691c287c306385dafc4906fa9b71
Marco Penguin <marcovnyc@gmail.com>
2018-01-21 01:08:43 -0500

    Fixing tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,7 +3,7 @@ import unittest
 from buzz import generator
 
 def test_sample_single_word():
-   xl = ('foo', 'bar', 'foobar')
+   l = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
     assert word in l
 
