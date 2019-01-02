ffd6650bafe2f185570da992a8ad31eaddfa0f1a
Rob van der Leek <robvanderleek@gmail.com>
2019-01-03 00:15:46 +0100

    Remove obsolete docker login option

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
