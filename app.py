411398579373906ecdd9765e2f00773f0f3f2fd1
Rob van der Leek <robvanderleek@gmail.com>
2019-01-28 09:02:32 +0100

    Remove signal handler

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import os
-import signal
 from flask import Flask
 from buzz import generator
 
 app = Flask(__name__)
-
-signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
