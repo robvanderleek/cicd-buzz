cfcb4e234b31ff6bd13fd4120199d6bee81d0abe
Marco Maldonado <marco@withfrank.org>
2019-05-29 16:07:19 -0400

    Added send their best

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,6 +1,6 @@
 import random
 
-buzz = ('fake news', 'continuous testing', 'continuous integration', 'Simple Reach ci di', 'Jenkins builds',
+buzz = ('they never send their best', 'fake news', 'continuous testing', 'continuous integration', 'Simple Reach ci di', 'Jenkins builds',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end', 'Make', 'Microservices')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'Make America Great Again', 
